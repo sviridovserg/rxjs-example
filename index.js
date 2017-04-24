6d977ae6e6304ff184c95f00d058f74d851a1c1f
Sergei Sviridov <sergei.sviridov@readify.net>
2017-04-24 11:23:22 +1000

    Add jquery, add simple request-response as callbacks, which is not correct in Rx

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-const getMessage = () => "Hello World";
-document.getElementById('output').innerHTML = getMessage();
+((Rx, jQuery) => {
+    'use strict';
+
+    const getMessage = () => "Hello World";
+    document.getElementById('output').innerHTML = getMessage();
+
+    
+    var requestStream = Rx.Observable.just('https://api.github.com/users');
+    requestStream.subscribe((requestUrl) => {
+        Rx.Observable.create((observer) => {
+            jQuery.getJSON(requestUrl)
+                .done((r) => observer.onNext(r))
+                .fail((jqXHR, status, error) => observer.onError(error))
+                .always(() => onserver.onComplete());
+        }).subscribe(() => {
+            
+        });
+    });
+})(Rx, jQuery);
+
+
