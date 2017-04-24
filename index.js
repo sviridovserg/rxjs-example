fba2f10268fa525a75142c08499b0ca98b06502b
Sergei Sviridov <sergei.sviridov@readify.net>
2017-04-24 12:21:10 +1000

    Add simple rendering

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 ((Rx, jQuery) => {
-    'use strict';
+    console.log('start');
+
     var refreshClickStream = Rx.Observable.fromEvent(jQuery('.refresh'), 'click');
 
     // Emited on startup and refresh button click
-    var requestStream = refreshClickStream.map(() => {
-        var offset = Math.floor(Math.random()*500);
-        return 'https://api.github.com/users?since=' + offset;
-    }).startWith('https://api.github.com/users');
+    var requestStream = refreshClickStream.startWith('startup click')
+        .map(() => {
+            var offset = Math.floor(Math.random()*500);
+            return 'https://api.github.com/users?since=' + offset;
+        });
 
 
     var responseStream = requestStream
-        .flatMap((requestUrl) => Rx.Observable.fromPromise(jQuery.getJSON(requestUrl)));
+        .flatMap((requestUrl) => Rx.Observable.fromPromise(jQuery.getJSON(requestUrl)))
+        .subscribe((r) => {
+            console.log('response');
+            renderUser(r[0], jQuery('.user-list-item'));
+        });
+
+    function renderUser(user, userElement) {
+        console.log(user.avatar_url);
+        //console.log(userElement.find('user-list-item')[0])
+        userElement.find('.user-avatar').attr('src',   user.avatar_url);
+        userElement.find('.user-name').text(user.login);
+    }
 })(Rx, jQuery);
 
 
