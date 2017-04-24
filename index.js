5451ae24dbfc64a091a82be190c5825b85e7ee5d
Sergei Sviridov <sergei.sviridov@readify.net>
2017-04-24 11:37:22 +1000

    Add button click stream and request to the server for it

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 ((Rx, jQuery) => {
     'use strict';
 
-    const getMessage = () => "Hello World";
-    document.getElementById('output').innerHTML = getMessage();
+    // Emitted on startup
+    var startupRequestStream = Rx.Observable.just('https://api.github.com/users');
+    var refreshClickStream = Rx.Observable.fromEvent(jQuery('.refresh'), 'click');
+
+    // Emited on refresh button click
+    var refreshOnRequestStream = requestStream.map(() => {
+        var offset = Math.floor(Math.random()*500);
+        return 'https://api.github.com/users?since=' + offset;
+    });
 
 
-
-    var requestStream = Rx.Observable.just('https://api.github.com/users');
-    var responseStream = requestStream
+    var responseStream = Rx.Observable.merge(startupRequestStream, refreshOnRequestStream)
         .flatMap((requestUrl) => Rx.Observable.fromPromise(jQuery.getJSON(requestUrl)));
-
 })(Rx, jQuery);
 
 
